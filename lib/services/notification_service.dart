94bc8331ee47b1fc7ec8585f11b03fda2dff3c3f
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-06-09 17:34:44 +0530

    Added firebase authentication

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -12,7 +12,7 @@ class NotificationService {
   static Future<void> initialize() async {
     tz.initializeTimeZones();
     try {
-      final String timeZoneName = await FlutterTimezone.getLocalTimezone();
+      final String timeZoneName = (await FlutterTimezone.getLocalTimezone()).identifier;
       tz.setLocalLocation(tz.getLocation(timeZoneName));
       debugPrint("NotificationService: Initialized timezone to $timeZoneName");
     } catch (e) {
